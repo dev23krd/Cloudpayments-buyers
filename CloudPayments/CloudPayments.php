915188f5bf58debb25aff066b4aecee78e02e68b
dev23krd <uskaman2000@yandex.ru>
2023-07-20 14:49:20 +0300

    Модуль для Buyers 1.4

---
+++ CloudPayments/CloudPayments.php
@@ -0,0 +1,128 @@
+<?php
+
+require_once 'api/buyers.php';
+
+class CloudPayments extends Buyers {
+
+    public function checkout_form($order_id, $button_text = null) {
+        if (empty($button_text)) {
+            $button_text = 'Оплатить';
+        }
+
+        $order            = $this->orders->get_order((int)$order_id);
+        $payment_method   = $this->payment->get_payment_method($order->payment_method_id);
+        $payment_settings = $this->payment->get_payment_settings($payment_method->id);
+        $payment_currency = $this->money->get_currency(intval($payment_method->currency_id));
+
+        $order_description = 'Оплата заказа №' . $order->id;
+
+        $total = $order->total_price - (max(0, $order->discount)) - (max(0, $order->coupon_discount));
+        if(!$order->separate_delivery)
+            $total += $order->delivery_price;
+
+        $amount         = floatval($this->money->convert($total, $payment_method->currency_id, false));
+	    $payment_scheme = $payment_settings['payment_scheme'];
+        $success_url    = $this->config->root_url . '/order/' . $order->url;
+        $fail_url       = $this->config->root_url . '/order/' . $order->url;
+
+        $params = array(
+            'publicId'    => $payment_settings['public_id'],  //id из личного кабинета
+            'description' => $order_description, //назначение
+            'amount'      => $amount, //сумма
+            'currency'    => $payment_currency->code, //валюта
+            'invoiceId'   => $order->id, //номер заказа  (необязательно)
+            'accountId'   => $order->email, //идентификатор плательщика (необязательно)
+            'skin'    => $payment_settings['skin'],
+            'data'        => array(
+                'name'          => $order->name,
+                'phone'         => $order->phone,
+                'cloudPayments' => array(),
+            )
+        );
+        if (intval($payment_settings['enable_kkt'])) {
+            $params['data']['cloudPayments']['customerReceipt'] = $this->get_receipt($order, $payment_settings, $payment_method->currency_id);
+        }
+
+        $lang   = $payment_settings['language'];
+        $params = json_encode($params);
+
+        $button = "<script src=\"https://widget.cloudpayments.ru/bundles/cloudpayments?cms=Simpla\"></script>" . PHP_EOL;
+        $button .= "<form id='simpla_cloudpayments_form' method='post'>";
+        $button .= "<input type='submit' class='btn btn--default btn--sm has-ripple' value='" . $button_text . "'>";
+        $button .= "</form>" . PHP_EOL;
+        $button .= <<<SCRIPT
+        <script>
+            (function(show_widget_callback) {
+                var form = document.getElementById('simpla_cloudpayments_form');
+                if (form.addEventListener) {
+                    form.addEventListener('click', show_widget_callback, false);
+                } else {
+                    form.attachEvent('onclick', show_widget_callback);
+                }
+            })(function(e) {
+                var evt = e || window.event; // Совместимость с IE8
+                if (evt.preventDefault) {  
+                    evt.preventDefault();  
+                } else {  
+                    evt.returnValue = false;  
+                    evt.cancelBubble = true;  
+                }
+                var widget = new cp.CloudPayments({language: '{$lang}'});
+                widget.{$payment_scheme}({$params}, '{$success_url}', '{$fail_url}');
+            });
+        </script>
+SCRIPT;
+
+        return $button;
+    }
+
+    private function get_receipt($order, $payment_settings, $currency_id) {
+        $tax_system   = $payment_settings['taxation_system'];
+        $receipt_data = array(
+            'Items'          => array(),
+            'amounts'        => array(),
+            'taxationSystem' => $tax_system,
+	        'calculationPlace'=>'www.'.$_SERVER['SERVER_NAME'],
+            'email'          => $order->email,
+            'phone'          => $order->phone
+        );
+        $electronic = 0;
+        $purchases = $this->orders->get_purchases(array('order_id' => intval($order->id)));
+        $vat       = $payment_settings['vat'];
+        if ($vat == 'none') {
+            $vat = '';
+        }
+        foreach ($purchases as $purchase) {
+            $price                   = $this->money->convert($purchase->price, $currency_id, false);
+            $price                   = number_format($price, 2, '.', '');
+            $receipt_data['Items'][] = array(
+                'label'    => trim($purchase->product_name . ' ' . $purchase->variant_name),
+                'price'    => floatval($price),
+                'quantity' => floatval($purchase->amount),
+                'amount'   => floatval($price * $purchase->amount),
+                'vat'      => $vat
+            );
+            $electronic = $electronic+floatval($price * $purchase->amount);
+        }
+        if ($order->delivery_price && !$order->separate_delivery) {
+            $delivery_method = $this->delivery->get_delivery($order->delivery_id);
+            $price           = $this->money->convert($order->delivery_price, $currency_id, false);
+            $price           = number_format($price, 2, '.', '');
+            $vat_delivery    = $payment_settings['vat_delivery'];
+            if ($vat_delivery == 'none') {
+                $vat_delivery = '';
+            }
+            $receipt_data['Items'][] = array(
+                'label'    => $delivery_method->name,
+                'price'    => floatval($price),
+                'quantity' => 1,
+                'amount'   => floatval($price),
+                'vat'      => $vat_delivery
+            );
+            $electronic = $electronic+floatval($price);
+        }
+        $receipt_data['amounts']['electronic'] = $electronic;
+
+        return $receipt_data;
+    }
+}
---
+++ CloudPayments/callback.php
@@ -0,0 +1,108 @@
+<?php
+// Работаем в корневой директории
+chdir('../../');
+require_once('api/buyers.php');
+$buyers = new Buyers();
+
+const CLOUDPAYMENTS_RESULT_SUCCESS             = 0;
+const CLOUDPAYMENTS_RESULT_ERROR_INVALID_ORDER = 10;
+const CLOUDPAYMENTS_RESULT_ERROR_INVALID_COST  = 11;
+const CLOUDPAYMENTS_RESULT_ERROR_NOT_ACCEPTED  = 13;
+const CLOUDPAYMENTS_RESULT_ERROR_EXPIRED       = 20;
+
+//Проверяем наличие обязательных параметров
+$action = isset($_GET['s_action']) ? strtolower($_GET['s_action']) : '';
+if (!in_array($action, array('check', 'pay', 'fail', 'refund', 'confirm', 'cancel'))) {
+    exit_with_error(CLOUDPAYMENTS_RESULT_ERROR_NOT_ACCEPTED);
+}
+if (empty($_POST['InvoiceId']) || empty($_POST['Amount'])) {
+    exit_with_error(CLOUDPAYMENTS_RESULT_ERROR_NOT_ACCEPTED);
+}
+
+// Выберем заказ из базы
+$order = $buyers->orders->get_order(intval($_POST['InvoiceId']));
+if (empty($order)) {
+    exit_with_error(CLOUDPAYMENTS_RESULT_ERROR_INVALID_ORDER);
+}
+
+// Получаем метод оплаты
+$payment_method = $buyers->payment->get_payment_method($order->payment_method_id);
+if (empty($payment_method)) {
+    exit_with_error(CLOUDPAYMENTS_RESULT_ERROR_NOT_ACCEPTED);
+}
+$payment_settings = unserialize($payment_method->settings);
+$payment_currency = $buyers->money->get_currency(intval($payment_method->currency_id));
+
+// Проверяем контрольную подпись
+$post_data    = file_get_contents('php://input');
+$check_sign   = base64_encode(hash_hmac('SHA256', $post_data, $payment_settings['secret_key'], true));
+$request_sign = isset($_SERVER['HTTP_CONTENT_HMAC']) ? $_SERVER['HTTP_CONTENT_HMAC'] : '';
+
+if ($check_sign !== $request_sign) {
+    exit_with_error(CLOUDPAYMENTS_RESULT_ERROR_NOT_ACCEPTED);
+};
+
+// Запросы связанные с оплатой
+$is_payment_callback = in_array($action, array('check', 'pay', 'confirm'));
+
+// Нельзя оплатить уже оплаченный заказ
+if ($is_payment_callback && $order->paid) {
+    exit_with_error(CLOUDPAYMENTS_RESULT_ERROR_NOT_ACCEPTED);
+}
+
+// Проверяем сумму заказа
+$amount = floatval($buyers->money->convert($order->total_price, $payment_method->currency_id, false));
+
+if ($_POST['Amount'] != $amount || $_POST['Amount'] <= 0) {
+    exit_with_error(CLOUDPAYMENTS_RESULT_ERROR_INVALID_COST);
+}
+
+if ($is_payment_callback) {
+    // Проверяем наличие товара
+    $purchases = $buyers->orders->get_purchases(array('order_id' => intval($order->id)));
+    foreach ($purchases as $purchase) {
+        $variant = $buyers->variants->get_variant(intval($purchase->variant_id));
+        if (empty($variant) || (!$variant->infinity && $variant->stock < $purchase->amount)) {
+            exit_with_error(CLOUDPAYMENTS_RESULT_ERROR_INVALID_COST);
+        }
+    }
+}
+
+if (($action == 'pay' && $_POST['Status'] == 'Completed') || $action == 'confirm') {
+    // Установим статус оплачен
+    $buyers->orders->update_order(intval($order->id), array('paid' => 1));
+
+    // Отправим уведомление на email
+    $buyers->notify->email_order_user(intval($order->id));
+    $buyers->notify->email_order_admin(intval($order->id));
+
+    // Спишем товары
+    $buyers->orders->close(intval($order->id));
+} else if ($action == 'refund' || $action == 'cancel') {
+    $note = $order->note;
+    if (!empty($note)) {
+        $note .= "\n";
+    }
+    if ($action == 'refund') {
+    $note .= 'Совершён полный возврат денежных средств.';
+    }
+    else $note .= 'Платеж отменен.';
+    // Установим статус не оплачен и запишем заметку
+    $buyers->orders->update_order(intval($order->id), array('paid' => 0, 'note' => $note));
+
+    // Отправим уведомление на email
+    $buyers->notify->email_order_user(intval($order->id));
+    $buyers->notify->email_order_admin(intval($order->id));
+}
+
+print_callback_response(CLOUDPAYMENTS_RESULT_SUCCESS);
+
+function print_callback_response($code) {
+    header('Content-Type: application/json');
+    echo json_encode(array('code' => $code));
+}
+
+function exit_with_error($code) {
+    print_callback_response($code);
+    die();
+}
